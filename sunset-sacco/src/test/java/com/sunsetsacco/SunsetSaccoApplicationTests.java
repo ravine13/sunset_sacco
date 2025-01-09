676d81dbff21c445833de080126027bb74c1e6e1
ravine13 <kuriaderek@gmail.com>
2025-01-09 17:37:07 +0300

    new: project set up

---
+++ sunset-sacco/src/test/java/com/sunsetsacco/SunsetSaccoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunsetsacco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunsetSaccoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
