8ebdba002d06e586541c8d91f0542156cf40d22f
ravine13 <kuriaderek@gmail.com>
2025-01-14 15:30:14 +0300

    new: authentication comtroller created

---
+++ sunset-sacco/src/main/java/com/sunsetsacco/controller/AuthController.java
@@ -0,0 +1,67 @@
+package com.sunsetsacco.controller;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sunsetsacco.repository.UserRepository;
+
+import model.AuthenticationResponse;
+
+@RestController
+public class AuthController {
+    
+    @Autowired
+    private AuthenicationManager authenicationManager;
+
+    @Autowired
+    private CustomUserDetailService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody Authentication authenticationRequest) throws Exception
+    try {
+        authenicationManager.authenticate(
+            new usernamePasswordAuthenticationToken(authenticationRequest.getUsername(),authenticationRequest.getPassword())
+        );
+        
+    } catch (BadCredentialsException e) {
+        throw new Exception("Incorrect username or password", e);
+    }
+    System.out.println("authenticate "+ authenticationRequest.getUsername()+"pass "+authenticationRequest.getPassword());
+
+    final UserDetails UserDetails = UserDetailsService.laodUsername(authenticationRequest.getPassword());
+    final String jwt = jwtUtil.generateToken(userDetails);
+
+    return ResponseEntity.ok(new AuthenticationResponse(jwt));
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody User user){
+        if(userRepository.findUserByUsername(user.getUsername()).isPresent()){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("username already exist");
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
+    }
+
+}
