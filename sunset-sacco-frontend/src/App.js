8b56adca38ed58f6fb1b6977b32aba4a007ee772
ravine13 <kuriaderek@gmail.com>
2025-01-16 14:52:49 +0300

    new: frontend set up. login and register pages created. tailwind and sweet aleart installed

---
+++ sunset-sacco-frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      <SignUp /> 
+    </div>
+  );
+}
+
+export default App;
