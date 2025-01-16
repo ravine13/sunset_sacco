0f4116f94804465bb1c64fadca12c3767febd27c
ravine13 <kuriaderek@gmail.com>
2025-01-16 16:29:53 +0300

    new: footer added

--- sunset-sacco-frontend/src/App.js
+++ sunset-sacco-frontend/src/App.js
@@ -1,18 +1,25 @@
-import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import Login from './pages/Login';
-// import Login from './pages/Home';
 import Navbar from './pages/Navbar';
+import Footer from './pages/Footer';
 
 function App() {
   return (
-    <div className="App">
-      {/* <Home /> */}
-      <Navbar />
-      <Login />
-      <SignUp /> 
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <main> {/* This wraps the content inside the main tag */}
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            {/* Add more routes here as needed */}
+          </Routes>
+        </main>
+        <Footer /> {/* Footer remains at the bottom */}
+      </div>
+    </Router>
   );
 }
 
